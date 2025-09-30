5f00ebec8cd6e2a395319f6ca508dc8f10b4b4af
User <User@LAPTOP-S4RR5VTH>
2025-10-01 00:10:29 +0630

    initial commit

---
+++ src/main/java/com/springboot/MovieSpringBoot/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.springboot.MovieSpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String showIndex() {
+	
+		return "index";
+	}
+	
+	@GetMapping("/index")
+	public String showIndex1() {
+	
+		return "index";
+	}
+	
+	
+	
+}
